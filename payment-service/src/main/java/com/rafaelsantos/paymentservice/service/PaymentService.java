d51678b794d25cc23f8c85c9bbf059d3f349b062
Rafael Santos <rafa.05125@gmail.com>
2024-02-24 19:36:10 -0300

    PaymentService, PaymentServiceImpl

--- payment-service/src/main/java/com/rafaelsantos/paymentservice/service/PaymentService.java
+++ payment-service/src/main/java/com/rafaelsantos/paymentservice/service/PaymentService.java
@@ -1,2 +1,8 @@
-package com.rafaelsantos.paymentservice.service;public class PaymentService {
+package com.rafaelsantos.paymentservice.service;
+
+import com.rafaelsantos.paymentservice.model.Payment;
+
+public interface PaymentService {
+
+    void sendPayment(Payment payment);
 }
--- payment-service/src/main/java/com/rafaelsantos/paymentservice/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/rafaelsantos/paymentservice/service/impl/PaymentServiceImpl.java
@@ -1,2 +1,16 @@
-package com.rafaelsantos.paymentservice.service;public class PaymentServiceImpl {
+package com.rafaelsantos.paymentservice.service.impl;
+
+import com.rafaelsantos.paymentservice.model.Payment;
+import com.rafaelsantos.paymentservice.service.PaymentService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+@Log4j2
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    @Override
+    public void sendPayment(Payment payment) {
+        log.info("PAYMENT_SERVICE_IMPL ::: Payment Received {}", payment);
+    }
 }
